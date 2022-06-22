45293e52bd15de13d91420e935cc6796e6cf5718
Chris Goad <cagoad@gmail.com>
2022-06-22 07:47:22 -0600

    misc

---
+++ public/instances/drop_on_top_i_2.mjs
@@ -0,0 +1,13 @@
+
+import {rs as generatorP} from '/generators/drop_on_top.mjs';
+
+let rs = generatorP.instantiate();
+
+rs.setName('drop_on_top_i_2');
+rs.initialDropColor = 'rgb(75,75,255)';
+rs.finalDropColor = 'rgb(0,0,255)';
+
+
+export {rs};
+
+

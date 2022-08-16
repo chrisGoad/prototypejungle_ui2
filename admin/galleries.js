1a47e536c5e85b88e4c9cc9f943b7c0bf40528d0
Chris Goad <cagoad@gmail.com>
2022-08-16 08:53:49 -0600

    misc

---
+++ admin/galleries.js
@@ -0,0 +1,26 @@
+/* wall driftweb shelter
+*/
+module.exports = {
+sections: [
+
+[
+
+ [0,'drop_light','generators',1,'Grids',{likes:50,posted:1,category:'top',variant:0}], 
+ [0,'drop_night','generators',1,'Quad Trees',{likes:50,posted:1,category:'top',variant:0}], 
+ [0,'web_random_radius','generators',1,'Drops',{likes:36,posted:1,category:'top',variant:2}], 
+ [0,'quad_3','generators',1,'Webs',{likes:72,posted:1,category:'top',variant:2}], 
+ //[0,'drop_whorls','generators',1,'Lines',{likes:45,posted:1,category:'top'}], 
+ [0,'grid_fade','generators',1,'Lines',{likes:532,posted:1,category:'top'}], 
+ [0,'grid_comet','generators',1,'Comet',{likes:57,posted:1,category:'grid'}], 
+ [0,'grid_ramp','generators',1,'Ramp',{likes:14,posted:1,category:'grid'}], 
+ //[0,'grid_superposition','generators',1,'Superposition',{likes:103,posted:1,category:'grid',sources:1}], 
+ [0,'grid_vortex','generators',1,'Vortex',{likes:10,posted:1,category:'grid'}], 
+ [0,'grid_enigma','generators',1,'Enigma',{likes:10,posted:1,category:'grid',sources:1}],
+ [0,'grid_cloudy_sky','generators',1,'Cloudy Sky',{likes:2,posted:1,category:'grid'}],
+      
+],
+
+]
+};
+	
+ 

f37406c29a771243e26374e89fc0cb509fbdeab5
Chris Goad <cagoad@gmail.com>
2022-09-07 17:40:24 -0600

    misc

--- public/instances/quad_0_3.mjs
+++ public/instances/quad_0_3.mjs
@@ -3,9 +3,9 @@ import {rs as generatorP} from '/generators/quad_0.mjs';
 
 let rs = generatorP.instantiate();
 
-rs.setName('quad_0_2');
-let levels = 7;
-rs.quadParams.levels = levels;
+rs.setName('quad_0_3');
+let levels = 3;
+rs.quadParams = {levels};
 //rs.quadParams.circleScale = 0.25;
 
 
@@ -14,28 +14,26 @@ let visibles = rs.quadParams.visibles = [];
 rs.addToArray(visibles,1,levels);
 rs.addToArray(visibles,1,levels);
 
-rs.quadParams.mangle = {'lengthen':.3,'twist':0.05*Math.PI,within:rs.canvasToRectangle()}
+rs.quadParams.manglee = {'lengthen':.3,'twist':0.05*Math.PI,within:rs.canvasToRectangle()}
+rs.quadParams.chance = 0.95;
+rs.quadParams.chance = 0.7;
 
-let strokeWidths = rs.quadParams.strokeWidths = [];
-rs.computeExponentials(strokeWidths,rs.quadParams.levels,0.2,.9);
-
-rs.quadStroke = function (qd) {
-  debugger;
-  return Math.random() > 0.5?'rgb(150,150,250)':'rgb(250,150,150)';
+rs.quadFill = function (qd) {
+  let rb= Math.random() > 0.5;
+  let v = Math.floor(255*Math.random());
+  let b = `rgb(${v},${v},${v})`;
+  let r = `rgb(${v},0,0)`;
+  return rb?r:b;
 }
+let strokeWidths = rs.quadParams.strokeWidths = [];
+
+rs.computeExponentials(strokeWidths,rs.quadParams.levels,0.14,.9);
+
+let strokes = rs.quadParams.strokes = [];
+rs.addToArray(strokes,'black',levels);
 
 rs.quadSplitParams = function (qd) {
    //let v = 0.7;
-   debugger;
-   let wd = this.width;
-   let hwd = 0.5*wd;
-   let pgon = qd.polygon;
-   let lft = pgon.left();
-   let fr = (lft + hwd) / wd;
-   console.log('fr',fr);
-   let fr0 = 0.3+fr * 2 * 0.6;
-      console.log('fr0',fr0);
-
    debugger;
    if (0 && this.sp) {
      return this.sp;
@@ -47,13 +45,8 @@ rs.quadSplitParams = function (qd) {
    //v= 0.4;
    let rs = this.randomizeFrom({ornt:['h','v'],fr0:v,fr1:v,fr2:v,fr3:v,fr4:v,fr5:v});
    //let rs = this.randomizeFrom({ornt:['h','v'],fr0:{low:.2,high:.8},fr1:{low:.2,high:.8},fr2:{low:.2,high:.8},fr3:{low:.2,high:.8},fr4:{low:.2,high:.8},fr5:{low:.2,high:.8}});
-   return rs;
-
    this.sp = rs;
-   rs = this.randomizeFrom({ornt:['h','h'],fr0:{low:.6,high:.6},fr1:.2,fr2:.2,fr3:.6,fr4:0.3,fr5:.2});
-  // return rs;
-   fr0 = 0.8
-   rs = this.randomizeFrom({ornt:['h','h'],fr0:fr0,fr1:.2,fr2:.2,fr3:.6,fr4:0.3,fr5:.2});
+   rs = this.randomizeFrom({ornt:['h','v'],fr0:.4,fr1:.4,fr2:.4,fr3:.6,fr4:0.4,fr5:.3});
    //console.log('ornt',rs.ornt,'fr0',rs.fr0);
    console.log('rs',rs);
   return rs;

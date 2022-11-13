1e5534792e32eda0982a790104aba9f72891e638
Chris Goad <cagoad@gmail.com>
2022-11-13 06:18:23 -0700

    misc

--- admin/altSections.js
+++ admin/altSections.js
@@ -151,6 +151,7 @@ sections:
  [0,'part2_0_43','instances',1,'Part2 0 43',{likes:0,posted:0,category:'quad'}], 
  [0,'part2_0_44','instances',1,'Part2 0 44',{likes:0,posted:0,category:'quad'}], 
  [0,'part2_0_45','instances',1,'Part2 0 45',{likes:0,posted:0,category:'quad'}], 
+ [0,'part2_0_46','instances',1,'Part2 0 46',{likes:0,posted:0,category:'quad'}], 
 
   [0,'part2_0_D_0','instances',1,'Partition 1',{likes:0,posted:0,category:'quad'}], 
    [0,'part2_0_D_7','instances',1,'Partition 2',{likes:0,posted:0,category:'quad'}], 

2d15c8312c1e9f584031b783e51c36f690f7803c
Chris Goad <cagoad@gmail.com>
2023-02-10 09:45:00 -0700

    misc

--- public/generators/reflected_path_0.mjs
+++ public/generators/reflected_path_0.mjs
@@ -44,12 +44,12 @@ rs.pstate = {pspace,cstate:initState}
 let vel = 1;
 
 
-//rs.addPath = function (fromSide,fromP,dir,line,inFront,addingTrailer) {
+//rs.addPath = function (fromSide,fromP,dir,line,inBack,addingTrailer) {
 rs.addPath = function (params) {
-  let {name,fromSide,fromP,dir,line,inFront,addingTrailer} = params;
-  let {stepsSoFar:ssf,ccircles,circleP,sides,ht,lineP,lines,pstate} = this;
+  let {name,fromSide,fromP,dir,line,inBack,addingTrailer} = params;
+  let {stepsSoFar:ssf,ecircles,ecircleP,sides,ht,lineP,lines,pstate,segs} = this;
   let {pspace,cstate} = pstate;
-  let ln = ccircles.length;
+  let ln = ecircles.length;
   let nm = name?name:'p'+ln;
   let maxH = 20;
   // debugger;
@@ -84,11 +84,12 @@ rs.addPath = function (params) {
   }
     
   if (!name) {
-    let circ = circleP.instantiate().show();
-    ccircles.push(circ);
+    let circ = ecircleP.instantiate().show();
+    ecircles.push(circ);
   //if ((!line)|| addingTrailer) {
     line = lineP.instantiate().show();
     lines.push(line);
+    segs.push(seg);
     //line.stroke = randomColor();
     
   }
@@ -97,11 +98,11 @@ rs.addPath = function (params) {
   pspace[nm] ={kind:'sweep',step:vel,min:0,max:dist/vel,interval:1,once:1};
   if (name) {
     let cs = cstate[nm];
-   //Object.assign(ist,{value:0,fromP,toP,fromSide,toSide,start:ssf,vec,dir,line,inFront});
+   //Object.assign(ist,{value:0,fromP,toP,fromSide,toSide,start:ssf,vec,dir,line,inBack});
     Object.assign(cs,{value:0,fromP,toP,fromSide,toSide,start:ssf,vec,dir,done:0,cstep:undefined});
     debugger;
   } else {
-    cstate[nm] = {value:0,fromP,toP,fromSide,toSide,start:ssf,vec,dir,line,inFront,trailerNeeded:!inFront,seg};
+    cstate[nm] = {value:0,fromP,toP,fromSide,toSide,start:ssf,vec,dir,line,inBack,trailerNeeded:!inBack,seg};
   }
 }
 
@@ -109,131 +110,39 @@ rs.atCycleEnd = function (nm) {
  let {pstate} = this;
   let {pspace,cstate} = pstate;
   let cs = cstate[nm];
-  let {dir,toP,toSide,line,inFront} = cs;
+  let {dir,toP,toSide,line,inBack} = cs;
  // let na = toSide%2?dir-Math.PI:Math.PI -dir;
   let na = toSide%2?-dir:Math.PI -dir;
-  //rs.addPath = function (fromSide,fromP,dir,line,inFront,addingTrailer) {
+  //rs.addPath = function (fromSide,fromP,dir,line,inBack,addingTrailer) {
 
-  this.addPath({name:nm,fromSide:toSide,fromP:toP,dir:na,line,inFront});
+  this.addPath({name:nm,fromSide:toSide,fromP:toP,dir:na,line,inBack});
  debugger;
 }
 let L  =rs.lineLength = 10;
 
-
-rs.adjustLine = function (line,pos,h) {
-  let {lineLength:L,hends0,hends1,vends0,vends1,d} = this;
-  let {index} = line;
-  let {x,y} = pos;
-  debugger;
-  let p = h?x:y;
-  let ae0 = p-0.5*L;
-  let ae1 = p+0.5*L;
-  let ends0 = h?hends0:vends0;
-  let ends1 = h?hends1:vends1;
-  let crc0 = ends0[index];
-  let crc1 = ends1[index];
-  if ((ae0 >d) && (ae1 > d)) {
-    line.hide();
-    crc0.hide();
-    crc1.hide();
-  } else if ((ae0 < -d) && (ae1 < -d)) {
-    line.hide();
-    crc0.hide();
-    crc1.hide();
-  } else {
-    ae1 = Math.min(ae1,d);
-    ae0 = Math.max(ae0,-d);
-    let ne0 = h?Point.mk(ae0-p,0):Point.mk(0,ae0-p);
-    let ne1 = h?Point.mk(ae1-p,0):Point.mk(0,ae1-p);
-    line.show();
-    line.setEnds(ne0,ne1);
-    crc0.moveto(pos.plus(ne0));
-    crc1.moveto(pos.plus(ne1));
-    crc0.show();
-    crc1.show();
-  
-    
-  }
-  line.update();
-}
-
-rs.adjustLines = function (y) {
- // debugger;
-  let {lineDataDown,lineDataUp} = this;
-  let lnd = lineDataDown.length;
-  for (let i = 0;i<lnd;i++) {
-    let ld = lineDataDown[i];
-    if (i ===1) {
-      debugger;
-    }
-   this.adjustLine(ld,y);
-  };
-  lineDataUp.forEach((ld) => {
-   this.adjustLine(ld,-y);
-  });
-}
- 
-   
-
-
-
-
-
-
-
-rs.addLine = function (h) {
-  let {lineLength:L,hlines,vlines,lineP,hends0,hends1,vends0,vends1,circleP1} = this;
-  debugger;
-  let line = lineP.instantiate();
-  let ends0 = h?hends0:vends0;
-  let ends1 = h?hends1:vends1;
-  if (h) {
-    line.setEnds(Point.mk(-0.5*L,0),Point.mk(0.5*L,0));
-  } else {
-    line.setEnds(Point.mk(0,-0.5*L),Point.mk(0,0.5*L));
-  }
-  let crc0 = circleP1.instantiate();
-  let crc1 = circleP1.instantiate();
-  ends0.push(crc0);
-  ends1.push(crc1);
-  //lineData.push(lineDat);
-  line.hide();
-  let lines = h?hlines:vlines;
-  let ln = lines.length;
-  line.index = ln;
-  lines.push(line);
-  line.update();
-  return  line;
-}     
- 
-
 rs.updateStateOfCC = function (n){
-  let {stepsSoFar:ssf,ends,ht,ccircles,pstate,lineLength:ll,trailerAdded} = this;
-  let circ = ccircles[n];
+  let {stepsSoFar:ssf,ends,ht,ecircles,pstate,lineLength:ll,trailerAdded} = this;
+  let circ = ecircles[n];
   let nm = 'p'+n;
   let {pspace,cstate} = pstate;
   let cs = cstate[nm];
-  let {value:v,done,fromP,toP,vec,dir,line,inFront,fromSide,trailerNeeded,seg} = cs;
- // debugger;
- /*
- let line = hlines[n];
-  if (done) {
-    line.hide();
-    return;
-  }
-  let v = od?-iv: iv;
-  */
+  let {value:v,done,fromP,toP,vec,dir,line,inBack,fromSide,trailerNeeded,seg} = cs;
   let   pos = fromP.plus(vec.times(v*vel));
 
   let toGo = pos.distance(toP);
   let lGone = pos.distance(fromP);
   line.show();
   seg.active = 1;
-  if (inFront) {
+ // line.stroke = 'rgba(250,250,250,.5)';
+  line.update();
+
+  if (inBack) {
     if (toGo >= ll) {
       seg.active = 0;
-    //  line.hide();
-    //  return;
+  //      line.stroke = 'rgba(0,0,250,.5)';
+        line.update();
+
+   //  return;
     } 
     let lnl = Math.min(toGo,ll);
     let epos = pos.plus(vec.times(lnl));
@@ -248,10 +157,10 @@ rs.updateStateOfCC = function (n){
     if (farOut && trailerNeeded) {
        debugger;
        cs.trailerNeeded = 0;
-       //rs.addPath = function (fromSide,fromP,dir,line,inFront,addingTrailer) {
-//rs.addPath = function (fromSide,fromP,dir,line,inFront,addingTrailer) {
+       //rs.addPath = function (fromSide,fromP,dir,line,inBack,addingTrailer) {
+//rs.addPath = function (fromSide,fromP,dir,line,inBack,addingTrailer) {
 
-       this.addPath({fromSide,fromP,dir,line,inFront:1,addingTrailer:1});
+       this.addPath({fromSide,fromP,dir,line,inBack:1,addingTrailer:1});
     }    
     let lnl = Math.min(lGone,ll);
     let epos = pos.difference(vec.times(lnl));
@@ -262,68 +171,53 @@ rs.updateStateOfCC = function (n){
   line.update();
   circ.moveto(pos);
   circ.update();
- /*
-  this.adjustLine(line,pos,1);
-
-  line.moveto(pos);
-  line.update();*/
 }
 
-rs.intersectLine = function (hline,vline) {
-  let L = this.lineLength;
-  let ph = hline.getTranslation();
-  let pv = vline.getTranslation();
-  if ((ph.x+0.5*L)<pv.x) { 
-    return;
-  }
-  if (pv.x<(ph.x-0.5*L)) { 
-    return;
-  }
-  if ((pv.y+0.5*L)<ph.y) { 
-    return;
-  }
-  if (ph.y<(pv.y-0.5*L)) { 
-    return;
-  }
-  return Point.mk(pv.x,ph.y);
-}
-
-rs.allIntersections = function () {
+rs.allIntersections = function (segs) {
   debugger;
-  let {hlines,vlines} = this;
+  let ln = segs.length;
   let ai = [];
-  hlines.forEach((h) => {
-    vlines.forEach((v) => {
-      let i = this.intersectLine(h,v);
-      if (i) {
-        ai.push(i);
+  for (let i=0;i<ln;i++) {
+    let segi = segs[i];
+    if (!segi.active) {
+      continue;
+    }
+    for (let j=i+1;j<ln;j++) {
+      let segj = segs[j];
+      if (!segj.active) {
+        continue;
       }
-    });
-  });
+ 
+      let ints = segi.intersect(segj);
+      if (ints) {
+        ai.push(ints);
+      }
+    }
+  };
   return ai;
 }
 
 rs.placeCircles = function () {
-  let {circles,circleP} = this;
-  let ai = this.allIntersections();
-  let cl = circles.length;
+  let {icircles,icircleP,segs} = this;
+  let ai = this.allIntersections(segs);
+  let cl = icircles.length;
   let ail = ai.length;
   if (ail > cl) {
     let nn = ail-cl;
     for (let j=0;j<nn;j++) {
-      let crc = circleP.instantiate();
-      circles.push(crc);
+      let crc = icircleP.instantiate();
+      icircles.push(crc);
     }
   }
   for (let k=0;k<ail;k++) {
-    let crc = circles[k];
+    let crc = icircles[k];
     crc.moveto(ai[k])
     crc.show();
     crc.update();
   }
   if (cl > ail) {
     for (let  k=cl;k<ail;k++) {
-      let crc = circles[k];
+      let crc = icircles[k];
       crc.hide();
       crc.update();
     }
@@ -335,6 +229,7 @@ rs.addSomePaths = function () {
   let theta = Math.random()*0.2*Math.PI-0.1*Math.PI
  theta = -0.1*Math.PI
 
+ //for (let i=1;i<3;i++) {
  for (let i=1;i<10;i++) {
     let tt =theta+0.0*i*Math.PI;
     this.addPath({fromSide:0,fromP:this.sideI2pnt(0,i/10),dir:tt});
@@ -345,49 +240,34 @@ let vStep=5;
 let nr = 80;
 rs.updateState = function () {
   //debugger;
-  let {stepsSoFar:ssf,numSteps,cycleTime,lines,ccircles,ht} = this;
+  let {stepsSoFar:ssf,numSteps,cycleTime,lines,ecircles,segs,ht} = this;
   //let lln = vlines.length;
-  let ccln = ccircles.length;
-  for (let i=0;i<ccln;i++) {
+  let ecln = ecircles.length;
+  for (let i=0;i<ecln;i++) {
     this.updateStateOfCC(i);
   }
   if (ssf % 30 === 0) {
     this.addSomePaths();
   }
-  return;
-  this.placeCircles()
-  if (Math.random() < 2) {
-    let rw = Math.floor(Math.random()*nr);
-    let r = (rw-nr/2) * (ht/nr);
-    debugger;
-    let od = (Math.random()<0.5)?1:0;
-    if (Math.random()<0.5) {
-      this.addLine(1);
-      this.addHpath(hln,r,od);
-    } else {
-      this.addLine(0);
-      this.addVpath(vln,r,od);
-    }
-  }
+  this.placeCircles();
 }
   
   
 rs.initProtos = function () {
-  let circleP = this.circleP = circlePP.instantiate();
-  circleP.fill = 'red';
-  circleP.fill = 'transparent';
-  circleP['stroke-width'] = 0;
-  circleP.dimension =0.005*this.ht;
-  circleP.dimension =0.01*this.ht;
-  let circleP1 = this.circleP1 = circlePP.instantiate();
-  circleP1.fill = 'blue';
-  circleP1['stroke-width'] = 0;
-  circleP1.dimension =0.01*this.ht;
+  let icircleP = this.icircleP = circlePP.instantiate();
+  icircleP.fill = 'red';
+  icircleP['stroke-width'] = 0;
+  icircleP.dimension =0.005*this.ht;
+  icircleP.dimension =0.01*this.ht;
+  let ecircleP = this.ecircleP = circlePP.instantiate();
+  ecircleP.fill = 'blue';
+  ecircleP['stroke-width'] = 0;
+  ecircleP.dimension =0.01*this.ht;
   let polygonP = this.polygonP = polygonPP.instantiate();
   polygonP.fill = 'blue';
   polygonP.fill = 'transparent';
     let lineP = this.lineP = linePP.instantiate();
-  lineP.stroke = 'white';
+  lineP.stroke = 'rgb(250,250,250)';
   lineP['stroke-width'] = .4;
 }  
 
@@ -408,17 +288,17 @@ rs.initialize = function () {
   this.addFrame();
   let lines = this.set('lines',arrayShape.mk());
   let icircles = this.set('icircles',arrayShape.mk());
-  let ccircles = this.set('ccircles',arrayShape.mk());
-  this.set('hends0',arrayShape.mk());
+  let ecircles = this.set('ecircles',arrayShape.mk());
+  this.set('segs',arrayShape.mk());
   this.set('vends0',arrayShape.mk());
   this.set('hends1',arrayShape.mk());
   this.set('vends1',arrayShape.mk());
  // this.addLine(1);
  
-  let lineData = this.lineData = [];
+  this.segs = [];
   let theta = 0.1*Math.PI;
-  //rs.addPath = function (fromSide,fromP,dir,line,inFront) {
-//rs.addPath = function (fromSide,fromP,dir,line,inFront,addingTrailer) {
+  //rs.addPath = function (fromSide,fromP,dir,line,inBack) {
+//rs.addPath = function (fromSide,fromP,dir,line,inBack,addingTrailer) {
   for (let i=1;i<0;i++) {
     this.addPath({fromSide:0,fromP:this.sideI2pnt(0,i/10),dir:theta});
     this.addPath({fromSide:2,fromP:this.sideI2pnt(2,i/10),dir:Math.PI-theta});
